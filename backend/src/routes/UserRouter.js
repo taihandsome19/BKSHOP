ae3b83600136cb65e9f3a057960c2d2cad6489d3
Nhnhanbt <nhnhanbnt@gmail.com>
2024-07-24 11:10:10 +0700

    Fix UserRouter

--- backend/src/routes/UserRouter.js
+++ backend/src/routes/UserRouter.js
@@ -9,9 +9,11 @@ router.get('/order/detail', UserController.order_detail);
 router.post('/updateInfo', UserController.updateInfo);
 router.post('/order', UserController.addToOrder);
 //------------------------------------------
-router.get('/', CartController.cart);
-router.post('/', CartController.addToCart);
-
+router.get('/cart', CartController.cart);
+router.post('/cart', CartController.addToCart);
+router.post('/increase', CartController.increaseQuantity);
+router.post('/decrease', CartController.decreaseQuantity);
+router.post('/remove', CartController.remove);
 
 // router.post('/order', UserController.makeOrder);
 
