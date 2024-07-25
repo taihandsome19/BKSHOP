855ab118a0a0c026c598c071fddb285fe763f6c0
Nhnhanbt <nhnhanbnt@gmail.com>
2024-07-25 10:01:23 +0700

    Thinh update

--- backend/src/services/support.js
+++ backend/src/services/support.js
@@ -1,6 +1,19 @@
-
 class Support {
-
+    createOrder = async (body) => {
+        const time = new Date()
+        const { payment, address, phonenum, status } = body
+        return {
+            address: address,
+            orderdate: `${time.getUTCDate()}/${time.getUTCMonth() + 1}/${time.getUTCFullYear()}`,
+            payment: {
+                method: payment,
+                status: false
+            },
+            phonenum: phonenum,
+            status: status,
+            items: {}
+        }
+    }
 };
 
 module.exports = new Support
